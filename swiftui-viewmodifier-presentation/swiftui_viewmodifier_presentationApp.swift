cb8871c877a125bced9272c812051a042dba7f7c
Pascal Edmond <pascal.edmond@bnpparibas.com>
2023-05-09 09:54:31 +0200

    Initial Commit

---
+++ swiftui-viewmodifier-presentation/swiftui_viewmodifier_presentationApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_viewmodifier_presentationApp.swift
+//  swiftui-viewmodifier-presentation
+//
+//  Created by Pascal EDMOND on 09/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_viewmodifier_presentationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
