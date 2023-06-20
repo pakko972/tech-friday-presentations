63b688ba423f62974b10d2bfeccfad64419833e6
Pascal Edmond <pascal.edmond@bnpparibas.com>
2023-06-20 15:42:32 +0200

    Playground: Add Tabs

--- Decks/DesignerWithSwiftUI/DesignWithSwiftUI-playground.swiftpm/ContentView.swift
+++ Decks/DesignerWithSwiftUI/DesignWithSwiftUI-playground.swiftpm/ContentView.swift
@@ -7,7 +7,26 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 struct ContentView: View {
+    
     var body: some View {
+        TabView {
+            accountList.tabItem {
+                Image(systemName: "square.dashed.inset.filled")
+                Text("Comptes")
+            }
+            Text("2").tabItem {
+                Image(systemName: "square.dashed.inset.filled")
+                Text("Suivi / Validation")
+            }
+            Text("3").tabItem {
+                Image(systemName: "square.dashed.inset.filled")
+                Text("Profil")
+            }
+        }
+        .tint(.bnpp)
+    }
+    
+    var accountList: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
